930784e1066a95701e59af3d3e1645000339e29b
Goun Lee <goun.lee@lge.com>
2023-10-20 14:17:06 +0900

    update path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/ilib-localespec-doc/docs">
+  <BrowserRouter basename="/ilib-localespec-doc">
     <App />
   </BrowserRouter>
 );
