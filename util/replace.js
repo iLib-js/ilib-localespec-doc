aaa67e909e80c70105c9d52f297c17f47556e8e1
Goun Lee <goun.lee@lge.com>
2023-11-15 09:48:04 +0900

    update comments

--- util/replace.js
+++ util/replace.js
@@ -3,11 +3,11 @@ var path = require('path');
 
 function usage() {
     console.log("Usage: replace.js [-h] [config] [toOutput]\n" +
-        "Generate number formats information files.\n\n" +
+        "Update files by using config data.\n\n" +
         "-h or --help\n" +
         "  this help\n" +
         "toDir\n" +
-        "  directory to output the package.json Default: ./tmp");
+        "  directory to place output files.: ./tmp");
     process.exit(1);
 }
 
@@ -31,19 +31,21 @@ indexJSFile(configData);
 function packageJSONFile(configData) {
     console.log("Modify package.json ...");
     var orgPkgData = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+
     orgPkgData["homepage"] = configData.packages.homepage;
     if (typeof configData.packages.ilibVersion !== 'undefined') {
         orgPkgData["dependencies"]["ilib"] = configData.packages.ilibVersion;
     }
+
     fs.writeFileSync(path.join(toOutput, "package.json"), JSON.stringify(orgPkgData, true, 2), "utf-8");
 }
 
 function indexJSFile(configData) {
     console.log("Modify src/index.js ...");
     var jsContent = fs.readFileSync("./src/index.js", "utf-8");
+
     var updated = jsContent.replace(regExp, `$1` + configData.index.basename + `$3`);
     fs.writeFileSync(path.join(toOutput, "index.js"), updated, "utf-8");
-    
 }
 
 console.log("Done");
